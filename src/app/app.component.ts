35cfaa227170e2239abeb741f6a3b11756d3c1c9
Abdulhussain Songerwala <abdulhussainsongerwala@Abdulhussains-MacBook-Pro.local>
2020-04-14 13:03:51 +0530

    Logic for calculations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'binary-calculator';
+  resDisplay: string;
+  operand1: string;
+  operand2: string;
+  operator: string;
+  firstOperandEntered: boolean;
+
+  constructor() {
+    this.resDisplay = '';
+    this.operand1 = '';
+    this.operand2 = '';
+    this.operator = '';
+    this.firstOperandEntered = false;
+  }
+
+  appendDigit(digit: string): void {
+    this.resDisplay += digit;
+    if (this.firstOperandEntered) {
+      this.operand2 += digit;
+    }
+  }
+
+  clear(): void {
+    this.resDisplay = '';
+    this.operand1 = '';
+    this.operand2 = '';
+    this.operator = '';
+    this.firstOperandEntered = false;
+  }
+
+  appendOperator(operator: string): void {
+    this.operand1 = this.resDisplay;
+    this.operator = operator;
+    this.resDisplay += operator;
+    this.firstOperandEntered = true;
+  }
+
+  evaluate(): void {
+    const op1 = Number.parseInt(this.operand1, 2);
+    const op2 = Number.parseInt(this.operand2, 2);
+    let result: number;
+    switch (this.operator) {
+      case '+':
+        result = op1 + op2;
+        break;
+      case '-':
+        result = op1 - op2;
+        break;
+      case '*':
+        result = op1 * op2;
+        break;
+      case '/':
+        result = op1 / op2;
+        break;
+    }
+    this.clear();
+    this.resDisplay = result.toString(2);
+  }
 }
